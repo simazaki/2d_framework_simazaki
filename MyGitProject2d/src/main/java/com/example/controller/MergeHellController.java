096cbd2a4bea57e72ccbfc969371460d71734c31
S0529 <S0529@21S0529>
2023-11-30 09:46:26 +0900

    できた

---
+++ MyGitProject2d/src/main/java/com/example/controller/MergeHellController.java
@@ -0,0 +1,19 @@
+package com.example.controller;
+import java.util.Random;
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA();
+		kadaiB();
+
+	}
+
+	static void kadaiB() {
+	//ここに課題を書く。メソッド名変えてね。
+		Random RandObj = new Random();
+		int n = RandObj.nextInt(100) + 1;
+		System.out.println(n+"が出ました");
+
+
+
+		}
+}
