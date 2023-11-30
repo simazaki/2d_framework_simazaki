0aed818e3f8c3f21fe8ab819645846f332a2959e
G0943 <G0943@DESKTOP-DKP1SJN>
2023-11-30 10:09:14 +0900

    aaaa

---
+++ MyGitProject2d/src/main/java/com/example/controller/MergeHellController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA();
+        kadaiB();
+	}
+
+		static void kadaiA() {
+
+			for(int i = 1; i <= 10; i++) {
+				System.out.println("現在" + i + "回目のループです。");
+			}
+		}}
