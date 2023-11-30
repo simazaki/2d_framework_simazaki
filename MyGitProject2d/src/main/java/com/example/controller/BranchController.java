8475e5be2997489cde057aff487a152e4df1ab96
S0529 <S0529@21S0529>
2023-11-30 10:28:50 +0900

    ssss

--- MyGitProject2d/src/main/java/com/example/controller/BranchController.java
+++ MyGitProject2d/src/main/java/com/example/controller/BranchController.java
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 package com.example.controller;
 
 public class BranchController {
 //sakai
 
 }
-=======
-package com.example.controller;
-
-public class BranchController {
-
-}
->>>>>>> branch 'master' of https://github.com/simazaki/2d_framework_simazaki.git
